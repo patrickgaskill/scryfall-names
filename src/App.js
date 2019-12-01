d4157cf7ab0760318a2e31c378ae46856fa06c2f
Patrick Gaskill <patrick.gaskill@gmail.com>
2019-12-01 16:03:42 -0600

    Rewrite to use hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,152 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useReducer } from "react";
+import {
+  Container,
+  Segment,
+  Header,
+  Form,
+  Message,
+  Progress
+} from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+
+const initialState = {
+  loading: false,
+  warnings: [],
+  error: null,
+  cards: [],
+  totalCards: null
+};
+
+function reducer(state, action) {
+  console.log(action.type, action);
+  switch (action.type) {
+    case "submit": {
+      return { ...initialState, loading: true };
+    }
+    case "success": {
+      const { cards, totalCards } = action;
+      return {
+        ...state,
+        cards: [...state.cards, ...cards],
+        totalCards
+      };
+    }
+    case "warning": {
+      return { ...state, warnings: [...state.warnings, ...action.warnings] };
+    }
+    case "finished": {
+      return { ...state, loading: false };
+    }
+    case "error": {
+      const { error } = action;
+      return { ...state, loading: false, error };
+    }
+    default: {
+      throw new Error();
+    }
+  }
+}
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [
+    { loading, warnings, error, cards, totalCards },
+    dispatch
+  ] = useReducer(reducer, initialState);
+
+  async function fetchScryfall(url) {
+    const response = await fetch(url);
+    const {
+      object,
+      details,
+      data,
+      has_more,
+      next_page,
+      total_cards,
+      warnings
+    } = await response.json();
+
+    if (object === "error") {
+      dispatch({ type: "error", error: details });
+      return;
+    }
+
+    dispatch({
+      type: "success",
+      cards: data.map(card => card.name),
+      totalCards: total_cards,
+      warnings
+    });
+
+    if (warnings) {
+      dispatch({ type: "warning", warnings });
+    }
+
+    if (has_more) {
+      window.setTimeout(() => fetchScryfall(next_page), 50);
+    } else {
+      dispatch({ type: "finished" });
+    }
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    dispatch({ type: "submit" });
+    try {
+      await fetchScryfall(`https://api.scryfall.com/cards/search?q=${query}`);
+    } catch (e) {
+      dispatch({ type: "error", error: e.message });
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container text>
+      <Segment basic>
+        <Header>Fetch a list of card names from Scryfall</Header>
+        <Form
+          onSubmit={handleSubmit}
+          warning={warnings.length > 0}
+          error={!!error}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Form.Field>
+            <Form.Input
+              loading={loading}
+              icon="search"
+              placeholder="Enter a Scryfall query, like t:goblin"
+              value={query}
+              onChange={e => setQuery(e.target.value)}
+            />
+          </Form.Field>
+        </Form>
+        {error && <Message error content={error} />}
+        {warnings.length > 0 && <Message warning list={warnings} />}
+      </Segment>
+      {cards.length > 0 && (
+        <div>
+          {totalCards && (
+            <Message attached>
+              {totalCards} {totalCards > 1 ? "cards" : "card"} found.
+            </Message>
+          )}
+          <Segment attached>
+            {loading && (
+              <Progress
+                indicating
+                total={totalCards}
+                value={cards.length}
+                attached="top"
+              />
+            )}
+            {cards.map(card => (
+              <Fragment key={card}>
+                <span>{card}</span>
+                <br />
+              </Fragment>
+            ))}
+          </Segment>
+        </div>
+      )}
+    </Container>
   );
 }
 
