f5b8e4f715a0eb850dbf6b64ee2b885db38f26ef
Patrick Gaskill <patrick.gaskill@gmail.com>
2021-08-24 23:09:16 -0500

    Rename component

--- components/ErrorAlert.tsx
+++ components/ErrorAlert.tsx
@@ -4,7 +4,7 @@ type Props = {
   error: string;
 };
 
-export default function WarningsAlert({ error }: Props): JSX.Element {
+export default function ErrorAlert({ error }: Props): JSX.Element {
   return (
     <div className="px-4 py-2 text-red-600 bg-red-500 bg-opacity-10 border-0 rounded-md">
       <div className="flex gap-4 items-center">
